5a2b77b1d41d929df233260e6b0534c8ac574a8a
Weisong Zheng <z00647347@gmail.com>
2026-09-17 19:36:58 +0800

    feat: 补全所有页面的路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,69 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Indexview from '@/views/index/index.vue'
+import Clazzview from '@/views/clazz/index.vue'
+import Deptview from '@/views/dept/index.vue'
+import EmpView from '@/views/emp/index.vue'
+import Logview from '@/views/log/index.vue'
+import Stuview from '@/views/stu/index.vue'
+import EmpReportView from '@/views/report/emp/index.vue'
+import stuReportview from '@/views/report/stu/index.vue'
+import LayoutView from '@/views/layout/index.vue'
+import LoginView from '@/views/login/index.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/index',
+      name: 'index',
+      component: Indexview
+    },
+    {
+      path: '/clazz',
+      name: 'clazz',
+      component: Clazzview
+    },
+    {
+      path: '/dept',
+      name: 'dept',
+      component: Deptview
+    },
+    {
+      path: '/emp',
+      name: 'emp',
+      component: EmpView
+    },
+    {
+      path: '/log',
+      name: 'log',
+      component: Logview
+    },
+    {
+      path: '/stu',
+      name: 'stu',
+      component: Stuview
+    },
+    {
+      path: '/report/emp',
+      name: 'empReport',
+      component: EmpReportView
+    },
+    {
+      path: '/report/stu',
+      name: 'stuReport',
+      component: stuReportview
+    },
+    {
+      path: '/layout',
+      name: 'layout',
+      component: LayoutView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    }
   ]
 })
 
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -25,15 +25,75 @@
       <el-container>
         <!-- 左侧菜单 -->
         <el-aside width="200px" class="aside">
-          <el-menu>
-            <el-sub-menu index="1">
+          <!-- 左侧菜单栏 -->
+          <el-menu router>
+            <!-- 首页菜单 -->
+            <el-menu-item index="/index">
+              <el-icon>
+                <Promotion />
+              </el-icon> 首页
+            </el-menu-item>
+
+            <!-- 班级管理菜单 -->
+            <el-sub-menu index="/manage">
               <template #title>
                 <el-icon>
-                  <message />
-                </el-icon>班级学员管理
+                  <Menu />
+                </el-icon> 班级学员管理
               </template>
-              <el-menu-item index="1-1">班级管理</el-menu-item>
-              <el-menu-item index="1-2">学员管理</el-menu-item>
+              <el-menu-item index="/clazz">
+                <el-icon>
+                  <HomeFilled />
+                </el-icon>班级管理
+              </el-menu-item>
+              <el-menu-item index="/stu">
+                <el-icon>
+                  <UserFilled />
+                </el-icon>学员管理
+              </el-menu-item>
+            </el-sub-menu>
+
+            <!-- 系统信息管理 -->
+            <el-sub-menu index="/system">
+              <template #title>
+                <el-icon>
+                  <Tools />
+                </el-icon>系统信息管理
+              </template>
+              <el-menu-item index="/dept">
+                <el-icon>
+                  <HelpFilled />
+                </el-icon>部门管理
+              </el-menu-item>
+              <el-menu-item index="/emp">
+                <el-icon>
+                  <Avatar />
+                </el-icon>员工管理
+              </el-menu-item>
+            </el-sub-menu>
+
+            <!-- 数据统计管理 -->
+            <el-sub-menu index="/report">
+              <template #title>
+                <el-icon>
+                  <Histogram />
+                </el-icon>数据统计管理
+              </template>
+              <el-menu-item index="/empReport">
+                <el-icon>
+                  <InfoFilled />
+                </el-icon>员工信息统计
+              </el-menu-item>
+              <el-menu-item index="/stuReport">
+                <el-icon>
+                  <Share />
+                </el-icon>学员信息统计
+              </el-menu-item>
+              <el-menu-item index="/log">
+                <el-icon>
+                  <Document />
+                </el-icon>日志信息统计
+              </el-menu-item>
             </el-sub-menu>
           </el-menu>
         </el-aside>
