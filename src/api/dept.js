7a3dcac067de77518aff265c8425df276901eb3f
Weisong Zheng <z00647347@gmail.com>
2026-09-18 13:18:45 +0800

    feat

--- src/api/dept.js
+++ src/api/dept.js
@@ -2,10 +2,15 @@ import request from "../utils/request";
 
 //查询全部部门信息
 export const queryAllApi = () => {
-  return request.get("/depts");
+    return request.get("/depts");
 };
 
 //新增部门
 export const addApi = (dept) => {
-  return request.post("/depts", dept);
+    return request.post("/depts", dept);
+};
+
+//根据ID查询
+export const querybyIdApi = (id) => {
+    return request.get(`/depts/${id}`);
 };
--- src/views/dept/index.vue
+++ src/views/dept/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, onMounted } from "vue";
-import { queryAllApi, addApi } from "../../api/dept";
+import { queryAllApi, addApi, querybyIdApi } from "../../api/dept";
 import { ElMessage } from "element-plus";
 
 const deptList = ref([]);
@@ -57,6 +57,19 @@ const addDept = () => {
   }
 };
 
+//编辑
+const edit = async (id) => {
+  formTitle.value = "编辑部门";
+  if (deptFormRef.value) {
+    deptFormRef.value.resetFields();
+  }
+  const result = await querybyIdApi(id);
+  if (result.code == 1) {
+    dialogFormVisible.value = true;
+    dept.value = result.data;
+  }
+}
+
 onMounted(() => {
   search();
 });
@@ -80,26 +93,16 @@ const rules = ref({
   <div class="container">
     <el-table :data="deptList" border style="width: 100%">
       <el-table-column type="index" label="序号" width="100" align="center" />
-      <el-table-column
-        prop="name"
-        label="部门名称"
-        width="260"
-        align="center"
-      />
-      <el-table-column
-        prop="updateTime"
-        label="最后操作时间"
-        width="300"
-        align="center"
-      />
+      <el-table-column prop="name" label="部门名称" width="260" align="center" />
+      <el-table-column prop="updateTime" label="最后操作时间" width="300" align="center" />
       <el-table-column label="操作">
         <template #default="scope">
-          <el-button type="primary" size="small"
-            ><el-icon> <EditPen /> </el-icon>编辑</el-button
-          >
-          <el-button type="danger" size="small"
-            ><el-icon> <Delete /> </el-icon>删除</el-button
-          >
+          <el-button type="primary" size="small" @click="edit(scope.row.id)"><el-icon>
+              <EditPen />
+            </el-icon>编辑</el-button>
+          <el-button type="danger" size="small"><el-icon>
+              <Delete />
+            </el-icon>删除</el-button>
         </template>
       </el-table-column>
     </el-table>
